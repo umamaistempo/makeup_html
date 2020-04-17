58d6d739453d65109224c7c4d634a86e2a40bd5c
Charlotte Lorelei de Oliveira <11341349+umamaistempo@users.noreply.github.com>
2020-04-17 18:07:51 +0200

    Mix project init

---
+++ lib/makeup_html.ex
@@ -0,0 +1,18 @@
+defmodule MakeupHtml do
+  @moduledoc """
+  Documentation for MakeupHtml.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MakeupHtml.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/makeup_html_test.exs
@@ -0,0 +1,8 @@
+defmodule MakeupHtmlTest do
+  use ExUnit.Case
+  doctest MakeupHtml
+
+  test "greets the world" do
+    assert MakeupHtml.hello() == :world
+  end
+end
